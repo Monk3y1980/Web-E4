ff86dd50fc7d411c6780f699790749ab68745fb6
Evgeny <eugen.eisner@gmail.com>
2021-04-24 08:31:15 +0200

    update task 5

--- 5.js
+++ 5.js
@@ -1,50 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+/*
+ Переписать консольное приложение из предыдущего юнита на классы.
+ Общие требования:
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <script>
-        /*
-        Переписать консольное приложение из предыдущего юнита на классы.
-        Общие требования:
-
-        Имена классов, свойств и методов должны быть информативными.
-        Соблюдать best practices
-        Использовать синтаксис ES6.
-        */
-        class Device {
-            constructor(name, plugin) {
-                this.name = name
-            }
-            getOn(plugin) {
-                if (this.plugin = plugin) {
-                    document.write("Включен потребляемая мощность: ");
-                } else {
-                    document.write("Выключен потребляемая мощность: ");
-                }
-            }
+ Имена классов, свойств и методов должны быть информативными.
+ Соблюдать best practices
+ Использовать синтаксис ES6.
+ */
+class Device {
+    constructor(name, plugin) {
+        this.name = name
+    }
+    getOn(plugin) {
+        if (this.plugin = plugin) {
+            document.write(`Включен ${this.name} потребляемая мощность: `);
+        } else {
+            document.write(`Выключен ${this.name} потребляемая мощность: `);
         }
+    }
+}
 
-        class ElectricalAppliance extends Device {
-            constructor(name, plugin, power){
-                super(getOn, name, plugin);
-            }
-            getPower(power, plugin) {
-            if (plugin === true) {
-                document.write(power / 1000 + "KWh" + "<br>");
-                return super.getOn();
-            } else {
-                document.write(power * 0);
-                return super.getOn();
-            }
+class ElectricalAppliance extends Device {
+    constructor(power, getOn, name) {
+        super(getOn, name);
+    }
+    getPower(power, getOn, name) {
+        if (getOn) {
+
+            return super.getOn(true) + document.write(power / 1000 + "KWh" + "<br>");
+        } else {
+            return super.getOn() + document.write(power * 0);
         }
-    </script>
-</body>
+    }
+}
 
-</html>
+const laptop = new ElectricalAppliance();
+// laptop.getOn(true);
+
+laptop.name = "Lenovo";
+laptop.getPower(20, true);
+
+const airpods = new ElectricalAppliance();
+//airpods.getOn(true);
+
+airpods.name = "Airpods";
+airpods.getPower(5, false);
